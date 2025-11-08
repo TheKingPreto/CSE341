c4cc801e9c140da24f26e9b7efe799394d9bb07d
TheKingPreto <lucasoliveirasilva2008@gmail.com>
2025-11-08 19:14:28 -0300

    swagger corrected

--- contacts-api/server.js
+++ contacts-api/server.js
@@ -15,7 +15,7 @@ const contactRoutes = require('./routes/contacts');
 app.use('/contacts', contactRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
